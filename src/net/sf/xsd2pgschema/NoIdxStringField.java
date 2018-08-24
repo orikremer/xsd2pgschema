96716d89540a8dbd64f51fcaa57dc0182b78da4a
Masashi Yokochi <yokochi47@gmail.com>
2018-08-24 12:57:15 +0900

    Update year

--- src/net/sf/xsd2pgschema/NoIdxStringField.java
+++ src/net/sf/xsd2pgschema/NoIdxStringField.java
@@ -1,6 +1,6 @@
 /*
     xsd2pgschema - Database replication tool based on XML Schema
-    Copyright 2014-2017 Masashi Yokochi
+    Copyright 2014-2018 Masashi Yokochi
 
     https://sourceforge.net/projects/xsd2pgschema/
 
